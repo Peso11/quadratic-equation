3aed33b0a50fc56afdcb5c441236e3005679198e
Peso11 <answer0333@gmail.com>
2018-02-23 02:39:19 +0300

    rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+var uravnenie =  {
+         answer: [0, 0],
+         a: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[0],
+         b: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[1],
+         c: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[2]
+
+     }
+     uravnenie.answer[0] = Math.round(( (-1) * uravnenie.b + Math.sqrt( uravnenie.b * uravnenie.b - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
+     uravnenie.answer[1] = Math.round(( (-1) * uravnenie.b - Math.sqrt( uravnenie.b * uravnenie.b - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
+     uravnenie.answer.sort((right, left) => {return right - left});
+     return uravnenie.answer;
+
+ }
