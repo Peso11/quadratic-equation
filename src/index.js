3cc4dd9c479f410273678aa7a00bd6850bb34b18
Peso11 <answer0333@gmail.com>
2018-02-23 03:01:37 +0300

    gotovo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ var uravnenie =  {
          c: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[2]
 
      }
-     uravnenie.answer[0] = Math.round(( (-1) * uravnenie.b + Math.sqrt( uravnenie.b * uravnenie.b - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
-     uravnenie.answer[1] = Math.round(( (-1) * uravnenie.b - Math.sqrt( uravnenie.b * uravnenie.b - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
-     uravnenie.answer.sort((right, left) => {return right - left});
-     return uravnenie.answer;
+     uravnenie.answer[0] = Math.round(( (-1) * uravnenie.b + Math.sqrt( Math.pow(uravnenie.b,2) - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
+     uravnenie.answer[1] = Math.round(( (-1) * uravnenie.b - Math.sqrt( Math.pow(uravnenie.b,2) - 4 * uravnenie.a * uravnenie.c)) / (2 * uravnenie.a));
+
+        var finalAnswer=uravnenie.answer.sort((right, left) => {return right - left});
+        return finalAnswer;
 
  }
